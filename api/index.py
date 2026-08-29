8e29282431f4dd2c4f6177e682f16326c3c0a880
tou-dev <tou.dev.12@gmail.com>
2026-08-29 17:14:38 +0200

    first deploy vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
 import os
 import sys
 
-# Append the absolute path of 'src' to sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+# Get the absolute path to the 'src' directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.abspath(os.path.join(current_dir, "..", "src"))
+
+# Insert 'src' at the very beginning of sys.path
+if src_dir not in sys.path:
+    sys.path.insert(0, src_dir)
 
 from main import app
